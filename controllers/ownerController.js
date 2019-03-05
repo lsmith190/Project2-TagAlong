abb20430e3855f2e8efc49cfd4210f0332fe1f84
lsmith190 <lindseyr.smith1217@gmail.com>
2019-03-05 11:44:45 -0500

    Added redirect to owners page from owners button

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -9,17 +9,21 @@ const ownerController = {
     new: (req, res) => {
         res.render('new')
     },
-    create: (req, res) => {
-        Owner.create(req.body).then(owner => {
-            res.redirect("/")
-        })
-    },
+    // create: (req, res) => {
+    //     Owner.create(req.body).then(owner => {
+    //         res.redirect("/")
+    //     })
+
+    //below redirects to page that now says "Show owners here"
+    create: ('/owners', (req, res) => {
+        res.send('Show all owners')
+    }),
     show: (req, res) => {
         console.log('Show owners')
     },
-    edit: (req, res) => {
-        console.log('edit owner')
-    },
+    edit: ('/edit', (req, res) => {
+        res.send('Edit owners here')
+    }),
     update: (req, res) => {
         console.log('updated owner')
     },
--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require("./connection.js");
 const Owner = require("../models/owner.js");
-const Pet = require("../models/pet.js");
-const Restaurant = require("../models/restaurant.js");
+// const Pet = require("../models/pet.js");
+// const Restaurant = require("../models/restaurant.js");
 //creates Schema for each model
 
 const lindsey = new Owner({
@@ -11,17 +11,17 @@ const lindsey = new Owner({
     petName: "Kenzie"
 });
 
-const kenzie = new Pet({
-    name: "Kenzie",
-    age: 3,
-    city: "Atlanta, GA",
-    breed: "Mutt",
-    ownerName: "Lindsey Smith"
-});
+// const kenzie = new Pet({
+//     name: "Kenzie",
+//     age: 3,
+//     city: "Atlanta, GA",
+//     breed: "Mutt",
+//     ownerName: "Lindsey Smith"
+// });
 
-const midway = new Restaurant({
-    name: "The Midway",
-    address: "552 Flat Shoals Ave SE, Atlanta, GA 30316",
-    rating: 4.4,
-    patio: true,
-});
+// const midway = new Restaurant({
+//     name: "The Midway",
+//     address: "552 Flat Shoals Ave SE, Atlanta, GA 30316",
+//     rating: 4.4,
+//     patio: true,
+// });
